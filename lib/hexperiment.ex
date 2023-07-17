2cb41022b1a546009ddc8db7f0e1d40900167ebe
Adam Kuffel <adam.kuffel@lemonbeat.com>
2023-07-17 21:43:39 +0200

    Created project using mix new . hexperiment

---
+++ lib/hexperiment.ex
@@ -0,0 +1,18 @@
+defmodule Hexperiment do
+  @moduledoc """
+  Documentation for `Hexperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hexperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hexperiment_test.exs
@@ -0,0 +1,8 @@
+defmodule HexperimentTest do
+  use ExUnit.Case
+  doctest Hexperiment
+
+  test "greets the world" do
+    assert Hexperiment.hello() == :world
+  end
+end
